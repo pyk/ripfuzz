651b9bd5e63e90fcb2c378b003855857b49dbd62
pyk <2213646+pyk@users.noreply.github.com>
2026-05-31 12:35:20 +0700

    test(evm::trace): add edge cases for vm.label interaction

--- fixtures/trace-inspector/src/LabelTrace.sol
+++ fixtures/trace-inspector/src/LabelTrace.sol
@@ -6,9 +6,15 @@ import {Vm} from "./Vm.sol";
 contract LabelTrace {
     Vm public constant VM = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
+    event LabeledTransfer(address indexed from, address indexed to, uint256 value);
+
     constructor() {
         VM.label(address(this), "SelfLabel");
         VM.label(address(0xBEEF), "BeefLabel");
+
+        VM.deal(address(0xBEEF), 100);
+        emit LabeledTransfer(address(0xBEEF), address(this), 1000);
+
         revert("constructor always reverts");
     }
 }
